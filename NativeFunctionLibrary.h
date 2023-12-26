1ab6bcc2b5e991dc074ef7272d6d8033ba1efbe2
jawadato <18325896+jawadato@users.noreply.github.com>
2023-12-26 20:09:18 +0600

    Update NativeFunctionLibrary.h

--- NativeFunctionLibrary.h
+++ NativeFunctionLibrary.h
@@ -1,13 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #pragma once
 
 
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "NativeFunctionLibrary.generated.h"
-
 
 
 /**
